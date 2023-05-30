d8bacffc5ba19f553855f4315918fd9e99d21f88
psrugh <commerce.peterugh@gmail.com>
2023-05-30 12:00:48 -0600

    Initial stab at it

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -3,9 +3,18 @@
  */
 const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
-  console.log("Performing bionic reading conversion on:", container);
-
-  /* Etc. etc. etc. your code etc. */
+  const ps = container.querySelectorAll('p');
+  Array.from(ps).forEach((p, i) => {
+    const words = p.innerText.split(' ');
+    p.innerHTML = '';
+    ionicContent = words.map((word) => {
+      const bold = document.createElement('strong');
+      const normal = document.createTextNode(`${word.substr(3)} `);
+      bold.innerText = word.substring(0,3);
+      p.appendChild(bold)
+      p.appendChild(normal)
+    })
+  });
 };
 
 // Allow global access so that this can be executed from the console.
